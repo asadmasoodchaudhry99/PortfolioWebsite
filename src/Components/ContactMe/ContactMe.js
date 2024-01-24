2f91b5d4b2b2a22779ed45407740661e39340d51
asadmasoodchaudhry99 <asadmchaudhry0@gmail.com>
2024-01-24 04:41:45 +0100

    MoreFixes

--- src/Components/ContactMe/ContactMe.js
+++ src/Components/ContactMe/ContactMe.js
@@ -277,7 +277,7 @@ useEffect(() => {
                     <div className="ResumeBtn SubmitContact">
                     <div className={`submitLoader ${isLoading ? "" : "hiddenformelement"}`}>
                     <svg className="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
-   <circle className="path" fill="none" stroke-width="6"  cx="33" cy="33" r="30"></circle>
+   <circle className="path" fill="none" strokeWidth="6"  cx="33" cy="33" r="30"></circle>
 </svg></div>
 <div className={`${isLoading ? "hiddenformelement" : ""}`}>
 <button type="submit" className="ResumeDownloadbtn">

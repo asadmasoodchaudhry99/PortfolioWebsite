148576dc411b25ce9ac25ed69bcedb482c287d17
asadmasoodchaudhry99 <asadmchaudhry0@gmail.com>
2024-01-24 04:39:14 +0100

    FixDiv

--- src/Components/ContactMe/ContactMe.js
+++ src/Components/ContactMe/ContactMe.js
@@ -276,8 +276,8 @@ useEffect(() => {
                   <Col lg={12}>
                     <div className="ResumeBtn SubmitContact">
                     <div className={`submitLoader ${isLoading ? "" : "hiddenformelement"}`}>
-                    <svg class="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
-   <circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+                    <svg className="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+   <circle className="path" fill="none" stroke-width="6"  cx="33" cy="33" r="30"></circle>
 </svg></div>
 <div className={`${isLoading ? "hiddenformelement" : ""}`}>
 <button type="submit" className="ResumeDownloadbtn">
